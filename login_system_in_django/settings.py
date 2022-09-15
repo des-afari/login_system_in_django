da1d3d28b6a49940d49fb12499e4ccf3382a2474
Desmond Afari <afaridesmond@gmail.com>
2022-09-15 16:58:59 +0000

    done

--- login_system_in_django/settings.py
+++ login_system_in_django/settings.py
@@ -127,6 +127,7 @@ STATICFILES_DIRS = [
 
 
 LOGIN_REDIRECT_URL = 'success'
+LOGOUT_REDIRECT_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
