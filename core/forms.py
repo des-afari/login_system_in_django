0fec937f140b963254e6d917a48eb7e89c7eb469
Desmond Afari <afaridesmond@gmail.com>
2022-09-15 09:26:17 +0000

    signup mobile style added

---
+++ core/forms.py
@@ -0,0 +1,8 @@
+from django import forms 
+
+class RegistrationForm(forms.Form):
+    first_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+    last_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+    email = forms.EmailField(label='', widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password_check = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Re-enter password'}))
--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
 # Create your views here.
+def signup(request):
+    form = RegistrationForm()
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'registration/signup.html', context)
